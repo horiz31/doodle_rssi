d6ecc4fc9b84c0183b9cff7a3a0843bbdcc1728e
Brad S <brad@horizon31.com>
2021-09-11 21:23:01 -0400

    add bat file

---
+++ config_doodle.bat
@@ -0,0 +1,15 @@
+@echo off
+
+if [%1]==[] goto usage
+@echo Copying files to %1...
+@echo If prompted for a password, please enter the root user's password on the Doodle radio
+scp -r etc\config\* root@%1:/etc/config/.
+scp -r usr\share\rpcd\acl.d\* root@%1:/usr/share/rpcd/acl.d/.
+@echo Restarting the rpcd service...
+ssh root@%1 /etc/init.d/rpcd restart
+goto :eof
+:usage
+@echo Missing argument, Usage: %0 ^<IP Address^>
+exit /B 1
+
+
